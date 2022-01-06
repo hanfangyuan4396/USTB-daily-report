f46d39a6a7fe9381daf368271a99cb0df5dc64ff
Han Fangyuan <hanfangyuan_ustb@163.com>
2022-01-06 21:40:16 +0800

    每次ping 与 submit读取最新的users.ini

--- python/ustb_report.py
+++ python/ustb_report.py
@@ -1,5 +1,6 @@
 import re
 import time
+import datetime
 import random
 import configparser
 import requests
@@ -8,11 +9,6 @@ from wechat_api import wechat_api
 
 
 url='https://isport.ustb.edu.cn/app.RSPWxClient/index.jsp'
-#添加请求头
-headers = {
-    'X-Requested-With': 'XMLHttpRequest',
-    'Cookie': 'JSESSIONID=1630476FC222D8D6C613426330495C15'
-}
 
 def str_to_dict(data_str):
     data_list = data_str.split('&')
@@ -22,36 +18,38 @@ def str_to_dict(data_str):
         data_dict[key] = vaule
     return data_dict
 
-config_parser = configparser.ConfigParser()
-config_parser.read(filenames='users.ini', encoding='utf-8')
-user_list = []
-for section in config_parser.sections():
-    user_dict = {}
-    user_dict['name'] = section
-    user_dict['cookie'] = config_parser[section]['cookie']
-    user_dict['data'] = str_to_dict(config_parser[section]['data'])
-    user_list.append(user_dict)
-print(user_list)
+def get_user_list():
+    config_parser = configparser.ConfigParser()
+    config_parser.read(filenames='users.ini', encoding='utf-8')
+    user_list = []
+    for section in config_parser.sections():
+        user_dict = {}
+        user_dict['name'] = section
+        user_dict['cookie'] = config_parser[section]['cookie']
+        user_dict['data'] = str_to_dict(config_parser[section]['data'])
+        user_list.append(user_dict)
+    return user_list
 
 def ping():
-    retry_number = 0
-    while retry_number < max_retry:
-        try:
-            requests.get(url, headers=headers)
-            break
-        except:
-            print('Ping failed.')
-            retry_number += 1
-    
-    if retry_number >= max_retry:
-        wechat_api.send_text_message('ping', 'ping failed')
+    for user_dict in get_user_list():
+        retry_number = 0
+        while retry_number < max_retry:
+            try:
+                requests.get(url, headers={'Cookie':user_dict['cookie']})
+                break
+            except:
+                print('Ping failed.')
+                retry_number += 1
+        
+        if retry_number >= max_retry:
+            wechat_api.send_text_message(f"{user_dict['name']} ping", 'ping failed')
         
 
 random_delay = True
 max_retry = 5
 max_delay = 5 # minutes
 retry_interval = 20 # seconds
-debug = False
+debug = True
 
 def one_submit(user_dict):
     if random_delay:
@@ -62,17 +60,17 @@ def one_submit(user_dict):
         response = requests.post(url, headers={'Cookie':user_dict['cookie']}, data=user_dict['data'])
         message = re.search(r'.*"message":\s*"(?P<message>.*?)"', response.text).group('message')
         print(message)
-        wechat_api.send_text_message('submit', message, touser=user_dict['name'])
         return message == '当天已上报You have submitted today.'
     except:
         print('Submit failed.')
         return False
 
 def submit():
-    for user_dict in user_list:
+    for user_dict in get_user_list():
         retry_number = 0
         while retry_number < max_retry:
             if one_submit(user_dict):
+                wechat_api.send_text_message(f"{user_dict['name']} submit", f'{datetime.datetime.now()} submit succeeded')
                 break
             else:
                 global random_delay
@@ -89,7 +87,6 @@ if __name__ == '__main__':
         random_delay = False
         retry_interval = 2
     scheduler = BlockingScheduler(timezone="Asia/Shanghai")
-    wechat_api.send_text_message('test', 'test')
     scheduler.add_job(ping, 'cron', minute='*/10')
-    scheduler.add_job(submit, 'cron', hour=20, minute=0)
-    # scheduler.start()
+    scheduler.add_job(submit, 'cron', hour=21, minute=38)
+    scheduler.start()
