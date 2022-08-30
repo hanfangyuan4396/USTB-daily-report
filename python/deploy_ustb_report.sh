71cc88a83089044448b291f16604252bb10a6574
hfy_ecs <hanfangyuan_ustb@163.com>
2022-08-30 12:44:24 +0800

    fix: 修复stop会关闭deploy脚本bug

--- python/deploy_ustb_report.sh
+++ python/deploy_ustb_report.sh
@@ -1,2 +1,2 @@
-cd ~/USTB-daily-report/python && git pull && sleep 5 && ./stop_ustb_report.sh && sleep 10 && ./start_ustb_report.sh 1 && sleep 5 && ./start_ustb_report.sh 0
+cd ~/USTB-daily-report/python && git pull && sleep 10 && ./stop_ustb_report.sh && sleep 10 && ./start_ustb_report.sh 1 && sleep 10 && ./start_ustb_report.sh 0
 
--- python/stop_ustb_report.sh
+++ python/stop_ustb_report.sh
@@ -1,2 +1,2 @@
-ps -ef | grep ustb_report | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep ustb_report | grep -v grep | grep -v deploy | awk '{print $2}' | xargs kill -9
 ps -ef | grep ustb_report
